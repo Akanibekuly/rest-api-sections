85198fc76935c915e776ed7e9e6ac4f28155fb8b
Akanibekuly <a.kanibekuly@gmail.com>
2020-11-02 15:22:31 +0600

    feat: user register class for db

--- my_codes_rest/section5/code/user.py
+++ my_codes_rest/section5/code/user.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 
 class User:
     def __init__(self, _id, username, password):
@@ -11,7 +12,7 @@ class User:
         connection=sqlite3.connect('data.db')
         cursor=connection.cursor()
 
-        query="SELCT * FROM users WHERE username=?"
+        query="SELECT * FROM users WHERE username=?"
         result=cursor.execute(query, (username,))
         row=result.fetchone()
         if row: 
@@ -27,7 +28,7 @@ class User:
         connection=sqlite3.connect('data.db')
         cursor=connection.cursor()
 
-        query="SELCT * FROM users WHERE id=?"
+        query="SELECT * FROM users WHERE id=?"
         result=cursor.execute(query, (_id,))
         row=result.fetchone()
         if row: 
@@ -37,3 +38,30 @@ class User:
         
         connection.close()
         return user
+
+class UserRegister(Resource):
+    parser=reqparse.RequestParser()
+    parser.add_argument('username',
+            type=str,
+            required=True,
+            help="This field cannot be left blank!"
+    )
+    parser.add_argument('password',
+            type=str,
+            required=True,
+            help="This field cannot be left blank!"
+    )
+    
+    def Post(self):
+        connection=sqlite3.connect('data.db')
+        cursor=connection.cursor()
+        data=UserRegister.parser.parse_args()
+
+        query="INSERT INTO users VALUES (NULL, ?, ?)"
+
+        cursor.execute(query, (data['username'],data['password']))
+
+        connection.commit()
+        connection.close()
+
+        return {'massage': 'User created succesfuuly!'}, 201 
